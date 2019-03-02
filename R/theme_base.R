75ea3c7579df512523aa5ac42c8023812389ce0a
Hayley Boyce <hayleyfboyce@gmail.com>
2019-03-02 12:23:09 -0800

    merge conflicts fixing

---
+++ R/theme_base.R
@@ -0,0 +1,26 @@
+theme_base <- function(colour_type){
+
+    theme_bw() +
+        theme(text = element_text(family = "sans", color = "black",
+                                  face = "plain", size = 16,
+                                  hjust = 0.5, vjust = 0.5, angle = 0,
+                                  lineheight = 1,
+                                  margin = margin(.7, .7,
+                                                  .7, .7),
+                                  debug = FALSE),
+              plot.title = element_text(family = "sans",
+                                        size = 22, color = "black"),
+              plot.background = element_rect(color = "white", fill = "white"),
+              panel.background = element_rect(color = "white", fill = "white"),
+              # axis options
+              axis.text = element_text(family = "sans", color = "black", size = 12),
+              axis.title = element_text(size = 16),
+              # legend options (for ratings plot)
+              legend.title = element_text(family = "sans", color = "black"),
+              legend.text = element_text(family = "sans", color = "black",
+                                         size = 12),
+              legend.position = "right",
+              legend.key = element_rect(colour = "black", linetype = "solid", size = 0.25),
+              legend.background = element_rect(color = "white", fill = "white",
+                                               linetype = "solid"))
+}
--- R/theme_trita.R
+++ R/theme_trita.R
@@ -4,16 +4,42 @@
 #' It modifies the colour of geometric objects (points, lines, etc) and the layout of the plot.
 #'
 
-theme_trita <-function(){
+source("R/theme_base.R")
 
-    #' @description: theme_prota() reformats a matplotlib plot with a Tritanopia friendly palette and theme.
+theme_trita <-function(colour_type){
+    #' @param colour_type either fill colour of colour depending on the geom of choice
+    #'
+    #' @description: theme_deutera() creates a theme with a Deuteranopia friendly colour palette that can be used on
+    #' both colour and fill aesthetics.
     #' Use it in conjuction with ggplot2
     #'
-    #' @usage: ggplot(data, aes(variable x, variable y)) + geom_point() + theme_trita()
+    #' @usage: ggplot(data, aes(variable x, variable y)) + geom_point() + theme_deutera()
     #'
-    #' @details: Create your plot via ggplot2 and apply our theme to enhance plots to be more readable for colourblind viewers.
+    #' @details: Create your plot via ggplot2 and apply our theme to enhance plots to be more readable for colourblind viewers
+    #' @export
+    #' @examples:
     #'
-    #' @return reformatted plot for tritanopia vision
+    #' p <- ggplot(mpg, aes(x = hwy, y = cty, fill = as.factor(cyl))) +geom_bar(stat = 'identity')
+    #' cowplot::plot_grid(p, p + theme_deutera(colour_type = 'fill'))
+    #'
+    #' p <- ggplot(mpg, aes(x = hwy, y = cty, colour = as.factor(cyl))) + geom_point(stat = 'identity')
+    #' cowplot::plot_grid(p, p +  theme_deutera(colour_type = 'colour'))
+    #'
+    #' ggplot(mpg, aes(hwy, colour = as.factor(cyl))) + geom_density()
+    #' cowplot::plot_grid(p, p +  theme_deutera(colour_type = 'colour'))
+    #'
+    #' @return reformatted plot for deuteranopia vision
 
-    return(NULL)
-}
+    theme_set(theme_base())
+
+    cb_palette <- c('#5F727A', '#FFB7C2', '#01919A', '#A8A8B4', '#BDE6F4','#4A3F45', '#B77B87', '#00585C', '#932929')
+
+    if (colour_type == 'fill'){
+        scale_fill_manual(values = cb_palette)
+    } else if (colour_type %in% c('colour', 'color')){
+        scale_colour_manual(values = cb_palette)
+    } else{
+        stop('colour_type expects either fill, colour or color.
+             For more information look at the documentation with ?theme_*.')
+    }
+    }
